5c36f2f5e08f928ffcf1a203edcafedcd6f82771
Johan Björn <johan.e.bjorn@gmail.com>
2017-05-30 16:03:34 +0200

    Auth0

--- factorio-admin-api/app.js
+++ factorio-admin-api/app.js
@@ -24,12 +24,18 @@ var jwtCheck = jwt({
 });
 
 app.use(jwtCheck);
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({message:'Missing or invalid token'});
+  }
+});
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
