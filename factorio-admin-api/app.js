e53019e8d1ccf0246100e8c948bdc5243897d8be
Johan Björn <johan.e.bjorn@gmail.com>
2017-05-30 14:46:15 +0200

    Auth0 implementation

--- factorio-admin-api/app.js
+++ factorio-admin-api/app.js
@@ -3,14 +3,31 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+
 var factorio = require('./factorio')();
 var rcon = require('./rcon-client')();
 
 var app = express();
 
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: "https://johanbjorn.eu.auth0.com/.well-known/jwks.json"
+    }),
+    audience: ['http://localhost:9000','qLiuvNGbX2UlX4BShUxzS9HyuTr8oQVV'],
+    issuer: "https://johanbjorn.eu.auth0.com/",
+    algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 app.use(bodyParser.json());
